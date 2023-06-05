4ecf4603eb2356a6e1c74a3a973f1407c4a03f8a
Saks777 <sb.sakenyunusov@gmail.com>
2023-06-05 12:39:31 +0500

    First commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+package Seminar_1_Dz_1;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    HotDrink coffe = new HotDrink("coffee", 0.2, true, 60, 100);
+    HotDrink tea = new HotDrink("tea", 0.4, true, 80, 40);
+    HotDrink capuchino = new HotDrink("capuchino", 0.5, false, 50, 150);
+    HotDrinksVendingMachine hotDrVenMach = new HotDrinksVendingMachine();
+    hotDrVenMach.addHotDrinks(coffe);
+    hotDrVenMach.addHotDrinks(capuchino);
+    hotDrVenMach.addHotDrinks(tea);
+
+    System.out.println(hotDrVenMach.getHotDrinks());
+
+    hotDrVenMach.shopHotDrinks();
+  }
+
+}
---
+++ VendingMachines.java
@@ -0,0 +1,11 @@
+package Seminar_1_Dz_1;
+
+public interface VendingMachines {
+
+  abstract void acceptingMoney();
+
+  abstract void productDelivery();
+
+  abstract void deliveryOfChange();
+
+}
